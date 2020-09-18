338fdbe7e915dd70163902b57ac38af0aa087865
ShantanuTiwari06 <shantanut710@gmail.com>
2020-09-18 20:17:21 +0530

    Note View Controller Setup

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -7,14 +7,115 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var noteInfoView: UIView!
+    @IBOutlet weak var noteImageViewView: UIView!
+    
+    @IBOutlet weak var noteNameLabel: UITextField!
+    @IBOutlet weak var noteDescriptionLabel: UITextView!
+    
+    @IBOutlet weak var noteImageView: UIImageView!
+    
+    var managedObjectContext: NSManagedObjectContext? {
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
+    var notesFetchedResultsController: NSFetchedResultsController<Note>!
+    var notes = [Note]()
+    var note: Note?
+    var isExisting = false
+    var indexPath: Int?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Load Data
+        if let note = note {
+            noteNameLabel.text = note.noteName
+            noteDescriptionLabel.text = note.noteDescription
+            noteImageView.image = UIImage(data: note.noteImage! as Data)
+        }
+        
+        if noteNameLabel.text != "" {
+            isExisting = true
+        }
+        
+        // Delegates
+        noteNameLabel.delegate =  self
+        noteDescriptionLabel.delegate = self
+        
+        // Styles
+        noteInfoView.layer.shadowColor = UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha:1.0).cgColor
+        noteInfoView.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        noteInfoView.layer.shadowRadius = 1.5
+        noteInfoView.layer.shadowOpacity = 0.2
+        noteInfoView.layer.cornerRadius = 2
+        
+        noteImageViewView.layer.shadowColor = UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha:1.0).cgColor
+        noteImageViewView.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+         noteImageViewView.layer.shadowRadius = 1.5
+         noteImageViewView.layer.shadowOpacity = 0.2
+         noteImageViewView.layer.cornerRadius = 2
+        
+        noteImageView.layer.cornerRadius = 2
+        noteNameLabel.setBottomBorder()
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    // Core Data
+    func saveToCoreData(completion: @escaping ()->Void) {
+        managedObjectContext!.perform {
+            do {
+                try self.managedObjectContext?.save()
+                completion()
+                print ("Note saved to core data.")
+            }
+            
+            catch let error {
+                print ("Could not save note to CoreData: \(error.localizedDescription)")
+            }
+        }
+    }
+    @IBAction func pickImageButtonWasPressed(_ sender: Any) {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        
+        let alertController = UIAlertController(title: "Add an Image", message: "Choose From", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        let photosLibraryAction = UIAlertAction(title: "Photos Library", style: .default) { (action) in
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        let savedPhotosAction = UIAlertAction(title: "Saved Photos Album", style: .default) { (action) in
+            pickerController.sourceType = .savedPhotosAlbum
+            self.present(pickerController, animated: true, completion: nil)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(photosLibraryAction)
+        alertController.addAction(savedPhotosAction)
+        alertController.addAction(cancelAction)
 
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
