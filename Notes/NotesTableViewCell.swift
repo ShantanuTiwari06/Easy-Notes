088b33a37804438a78ebbfb16c02e867509ba32f
ShantanuTiwari06 <shantanut710@gmail.com>
2020-09-18 10:41:52 +0530

    Edit Notes Table View Controller And Core Data

---
+++ Notes/NotesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotesTableViewCell.swift
+//  Notes
+//
+//  Created by Shantanu Tiwari on 18/09/20.
+//  Copyright © 2020 Shantanu Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
